299f67c9b9bdcb7a2fe716a51bdfb3ca4e61c443
denilsondev <denilsoncdc10@gmail.com>
2020-10-07 22:21:04 -0300

    feat: implementing methods

--- src/DevIO.Api/Configuration/DependencyInjectionConfig.cs
+++ src/DevIO.Api/Configuration/DependencyInjectionConfig.cs
@@ -1,4 +1,6 @@
 ﻿using DevIO.Business.Intefaces;
+using DevIO.Business.Notificacoes;
+using DevIO.Business.Services;
 using DevIO.Data.Context;
 using DevIO.Data.Repository;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,6 +17,10 @@ namespace DevIO.Api.Configuration
         {
             services.AddScoped<MeuDbContext>();
             services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IFornecedorService, FornecedorService>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+
+            services.AddScoped<INotificador, Notificador>();
 
             return services;
         }
