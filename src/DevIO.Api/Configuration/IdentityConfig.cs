1ceea57186f1139e4cf56ab711c0ed384362df22
denilsondev <denilsoncdc10@gmail.com>
2020-12-14 22:18:42 -0300

    feat: implements identity

--- src/DevIO.Api/Configuration/IdentityConfig.cs
+++ src/DevIO.Api/Configuration/IdentityConfig.cs
@@ -1,4 +1,5 @@
 ﻿using DevIO.Api.Data;
+using DevIO.Api.Extensions;
 using DevIO.Data.Context;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -21,6 +22,7 @@ namespace DevIO.Api.Configuration
             services.AddDefaultIdentity<IdentityUser>()
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddErrorDescriber<IdentityMensagemPortugues>()
                 .AddDefaultTokenProviders();
 
             return services;
---
+++ src/DevIO.Api/Extensions/IdentityMensagemPortugues.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Extensions
+{
+    public class IdentityMensagemPortugues: IdentityErrorDescriber
+    {
+        public override IdentityError DefaultError()
+        {
+            return new IdentityError
+            {
+                Code = nameof(DefaultError),
+                Description = "Ocorreu um erro desconhecido"
+            };
+        }
+
+    }
+}
