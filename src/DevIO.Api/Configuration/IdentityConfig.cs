508b1a2e8ffca2a89a588b7d668b6bc1d9c87359
denilsondev <denilsoncdc10@gmail.com>
2020-12-14 21:06:58 -0300

    feat: identity config

--- src/DevIO.Api/Configuration/IdentityConfig.cs
+++ src/DevIO.Api/Configuration/IdentityConfig.cs
@@ -1,5 +1,6 @@
 ﻿using DevIO.Api.Data;
 using DevIO.Data.Context;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,7 +16,12 @@ namespace DevIO.Api.Configuration
         public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
 
             return services;
         }
