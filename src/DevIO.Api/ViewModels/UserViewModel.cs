28a5b6100d1153ab647a8237b6d8a109e670214a
denilsondev <denilsoncdc10@gmail.com>
2020-12-22 18:58:55 -0300

    feat: return more data about user token

--- src/DevIO.Api/ViewModels/UserViewModel.cs
+++ src/DevIO.Api/ViewModels/UserViewModel.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace DevIO.Api.ViewModels
@@ -30,4 +31,24 @@ namespace DevIO.Api.ViewModels
         public string Password { get; set; }
     }
 
+}
+
+public class UserTokenViewModel
+{
+    public string Id { get; set; }
+    public string Email { get; set; }
+    public IEnumerable<ClaimViewModel> Claims { get; set; }
+}
+
+public class LoginResponseViewModel
+{
+    public string AccessToken { get; set; }
+    public double Expirein { get; set; }
+    public UserTokenViewModel UserToken { get; set; }
+}
+
+public class ClaimViewModel
+{
+    public string Value { get; set; }
+    public string Type { get; set; }
 }
